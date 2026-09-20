3b4b3c69249aa56ee5a181418162ae7fc7426d88
Andrii Sych <andrey23sych@gmail.com>
2026-09-20 15:16:02 +0200

    fix: configure host for railway

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -12,7 +12,7 @@ server.register(inquiriesRoutes);
 
 const start = async () => {
   try {
-    await server.listen({ port: env.PORT });
+    await server.listen({ port: env.PORT, host: '0.0.0.0' });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
